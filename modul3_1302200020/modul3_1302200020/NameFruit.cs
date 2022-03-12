e2fd614e828df060ade3b09ad5d85df6e84b1ebd
Heiasan <abdillahhasan130@gmail.com>
2022-03-12 16:16:37 +0700

    menambahkan class KodeBuah
    Abdillah Hasan_1302200020

---
+++ modul3_1302200020/modul3_1302200020/NameFruit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul3_1302200020
+{
+    internal class NameFruit
+    {
+        private string[] namabuah = { "Apel", "Aprikot", "Alpukat", "Pisang", "Paprika", "Blackberry", "Ceri", "Kelapa", "Jagung", "Kurma", "Durian", "Anggur", "Melon", "Semangka" };
+        private string[] kodeBuah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "O00" };
+
+        public void getNameFruit()
+        {
+            int panjang = namabuah.Length;
+            for (int i = 0; i < panjang; i++)
+            {
+                Console.WriteLine(namabuah[i] + " " + kodeBuah[i]);
+            }
+        }
+    }
+}
---
+++ modul3_1302200020/modul3_1302200020/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace modul3_1302200020
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            NameFruit nama = new NameFruit();
+            nama.getNameFruit();
+        }
+    }
+}
